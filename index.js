b66ca2a1b94547b18452a472ef2e15555ef27743
Mohammed Noori <kmanoorib@gmail.com>
2020-02-27 04:36:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ class Circle {
     this.radius = radius;
   }
   
-
+  set diameter() {
+    this.diameter = this.radius * 2;
+  }
   get diameter() {
     return this.radius * 2;
   }
   
   get circumference() {
     return diameter() * 3.14;
+  }
+  
+  get area() {
+    
   }
 }
